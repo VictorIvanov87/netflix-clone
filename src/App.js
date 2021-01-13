3a7b3b106ba75bcffa59a188b5fe277923f6c334
Victor Hristov <victor.hristov@mentormate.com>
2021-01-14 01:55:31 +0200

    Get end render all lists of movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row';
+import endpoints from './endpoints';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		{endpoints.map((endpoint, i) => {
+			return(<Row title={endpoint.title} fetchUrl={endpoint.fetchUrl} key={i}/>)	
+		})}
     </div>
   );
 }

53a62c65d8bbb117444ef27b3c1dec4b8bc26b19
Victor Hristov <victor.hristov@mentormate.com>
2021-01-16 01:12:16 +0200

    Add more genres

--- src/endpoints.js
+++ src/endpoints.js
@@ -6,6 +6,14 @@ const endpoints = [
 	{title: 'Top Rated', fetchUrl: `/movie/top_rated?api_key=${API_KEY}&language=en-US`, isLargeRow: false},
 	{title: 'Popular', fetchUrl: `/movie/popular?api_key=${API_KEY}&language=en-US`, isLargeRow: false},
 	{title: 'Upcoming', fetchUrl: `/movie/upcoming?api_key=${API_KEY}&language=en-US`, isLargeRow: false},
+	{title: 'Comedy', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=en-US`, isLargeRow: false},
+	{title: 'Action', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=en-US`, isLargeRow: false},
+	{title: 'Drama', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=18&language=en-US`, isLargeRow: false},
+	{title: 'Horror', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=en-US`, isLargeRow: false},
+	{title: 'Documentary', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=99&language=en-US`, isLargeRow: false},
+	{title: 'Animation', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=16&language=en-US`, isLargeRow: false},
+	{title: 'Mistery', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=9648&language=en-US`, isLargeRow: false},
+	{title: 'Family', fetchUrl: `/discover/movie?api_key=${API_KEY}&with_genres=10751&language=en-US`, isLargeRow: false},
 ]
 
 export default endpoints;
